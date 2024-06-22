70c33a952832c38874e73865a1958c3a2c9b02eb
Bruce <brucedev94@gmail.com>
2024-06-22 11:03:22 -0300

    Create main.py

---
+++ Exercicios/main.py
@@ -0,0 +1,13 @@
+
+# Solicita ao usuário que insira um valor para 'A' e o converte para um número.
+a = eval(input("Informe o primeiro valor: "))
+
+# Solicita ao usuário que insira um valor para 'B' e o converte para um número.
+b = eval(input("Informe o segundo valor: "))
+
+# Realiza as operações matemáticas e imprime os resultados.
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
