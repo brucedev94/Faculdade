849ed7943eef270cfcbd84577ab94fdeb4568e50
Bruce <brucedev94@gmail.com>
2024-07-21 14:15:57 -0300

    Create Qual é a área.py

---
+++ Exercícios/Qual é a área.py
@@ -0,0 +1,8 @@
+print("Controle de Terrenos")
+print("-" * 30)
+
+largura = float(input("Largura (m):"))
+comprimento = float(input("Comprimento (m):"))
+area = largura * comprimento
+
+print("A área de um terreno ", largura, "X", comprimento, "é de ", area, "m².")
