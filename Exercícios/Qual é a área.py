06cacaa2743f3085670bf18963d3ca026e2364d2
Bruce <brucedev94@gmail.com>
2024-09-24 20:48:55 -0300

    Novaatt

--- Exercícios/Qual é a área.py
+++ Exercícios/Qual é a área.py
@@ -1,8 +1,11 @@
+# Monstra o título do assunto.
 print("Controle de Terrenos")
 print("-" * 30)
 
+# Solicita ao usuário que insira o valor da largura e comprimento e o converte em um número.
 largura = float(input("Largura (m):"))
 comprimento = float(input("Comprimento (m):"))
 area = largura * comprimento
 
+# Monstra o resultado da operacão resultando no valor área.
 print("A área de um terreno ", largura, "X", comprimento, "é de ", area, "m².")
